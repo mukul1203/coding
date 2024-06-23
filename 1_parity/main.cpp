fc8a6c75b0bca3a3ad8ccbd2b30245da41e67f3e
Mukul Agrawal <mukuluec@gmail.com>
2024-06-23 03:40:18 +0000

    Usage of catch tests working. But compile time too high

--- 1_parity/main.cpp
+++ 1_parity/main.cpp
@@ -1,18 +1,54 @@
 #include <iostream>
+#include <cassert>
+#include "../test_framework/catch_amalgamated.hpp"
+
 using namespace std;
 
-template<typename T>
-bool parity(T val) {
-    bool ret = false;
-    while(val) {
-        ret ^= (val & 0x1);
-        val >>= 1;
+namespace v1
+{
+    template <typename T>
+    bool parity(T val)
+    {
+        bool ret = false;
+        while (val)
+        {
+            ret ^= (val & 0x1);
+            val >>= 1;
+        }
+        return ret;
     }
-    return ret;
 }
 
-int main() {
-    int x = 7;
-    cout<<"parity of " << x << " i.e. "<< hex << x <<" is "<< parity(x)<<endl;
-    return 0;
+namespace v2
+{
+    template <typename T>
+    bool parity(T val)
+    {
+        bool ret = false;
+        while (val)
+        {
+            val = val & (val - 1);
+            ret = !ret;
+        }
+        return ret;
+    }
 }
+
+namespace mukul = v1;
+
+TEST_CASE("Parity check", "[parity]")
+{
+    REQUIRE(mukul::parity(7) == 1);
+    REQUIRE(mukul::parity(3) == 1);
+}
+
+// int main()
+// {
+//     int x = 7;
+//     cout << "parity of " << x << " i.e. " << hex << x << " is " << mukul::parity(x) << endl;
+//     for (int i = 0; i < 1000; ++i)
+//     {
+//         assert(v1::parity(i) == mukul::parity(i));
+//     }
+//     return 0;
+// }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Hello world!
+
+Downloaded catch test framework files from 
+https://github.com/catchorg/Catch2/blob/devel/extras/catch_amalgamated.cpp
+https://github.com/catchorg/Catch2/blob/devel/extras/catch_amalgamated.hpp
