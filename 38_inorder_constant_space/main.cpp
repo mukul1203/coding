41ed863649d3cf96555f54edc116b98b8ca72b29
Mukul Agrawal <mukuluec@gmail.com>
2024-09-03 16:06:16 +0000

    Could make out the logic, but can't implement till we have node with parent ptr. Skipping

---
+++ 38_inorder_constant_space/main.cpp
@@ -0,0 +1,14 @@
+#include "binary_tree/binary_tree.hpp"
+#include <vector>
+
+// TODO: need to implement tree with nodes having parent pointers
+//  template <typename T>
+//  vector<node_ptr<T>> inorder(const node_ptr<T> &root)
+//  {
+//      vector<node_ptr<T>> res;
+//      auto cur = root, prev = nullptr;
+//      while (cur)
+//      {
+//      }
+//      return res;
+//  }
